6160f5b0c38d3d254346fd5f01971cfa03c65b3f
Admin <diegovaloriza@gmail.com>
2024-09-30 22:01:45 +0200

    No se si esta bien

---
+++ TheJavengers_IntellJ/src/MiclassTest.java
@@ -0,0 +1,11 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class MiclassTest {
+    @Test
+    public void testSum() {
+        int resultado = 2 + 3;
+        assertEquals(5, resultado);
+}
+
+
