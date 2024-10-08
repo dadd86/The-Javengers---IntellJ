8dd14c66afa1975a8580785b555a129aa1f21bc0
Admin <diegovaloriza@gmail.com>
2024-10-08 17:05:33 +0200

    Test

--- TheJavengers_IntellJ/src/TheJavengers/Escapadas.java
+++ TheJavengers_IntellJ/src/TheJavengers/Escapadas.java
@@ -3,5 +3,7 @@ package TheJavengers;
 public class Escapadas {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
     }
+
 }
