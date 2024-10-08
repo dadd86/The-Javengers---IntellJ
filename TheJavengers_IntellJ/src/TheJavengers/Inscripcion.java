80ba80464c63bcb00ea25769841ff3a5f7952919
msanchezelv <msanchezelv@uoc.edu>
2024-10-08 20:17:55 +0200

    Update Inscripcion.java

--- TheJavengers_IntellJ/src/TheJavengers/Inscripcion.java
+++ TheJavengers_IntellJ/src/TheJavengers/Inscripcion.java
@@ -52,7 +52,7 @@ public class Inscripcion {
     // Método para cancelar inscripción
     public boolean cancelarInscripcion() {
         LocalDate fechaExcursion = excursion.getFecha();
-        return fechaInscripcion.isBefore(fechaExcursion);  // Solo se puede cancelar si la fecha de inscripción es anterior a la fecha de la excursión
+        return fechaInscripcion.isBefore(fechaExcursion);
     }
 
     // Método para mostrar una inscripción
