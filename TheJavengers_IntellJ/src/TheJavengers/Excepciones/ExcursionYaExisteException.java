12987961fe8918ec51e7066c2bf41c6da98f39c6
Raul Morales <ramoraleloz@gmail.com>
2024-10-15 19:53:29 +0200

    MVC cambios

---
+++ TheJavengers_IntellJ/src/TheJavengers/Excepciones/ExcursionYaExisteException.java
@@ -0,0 +1,7 @@
+package TheJavengers.Excepciones;
+
+public class ExcursionYaExisteException extends Exception {
+    public ExcursionYaExisteException(String mensaje) {
+        super(mensaje);
+    }
+}
--- TheJavengers_IntellJ/src/TheJavengers/modelo/Socio.java
+++ TheJavengers_IntellJ/src/TheJavengers/modelo/Socio.java
@@ -1,4 +1,5 @@
-package TheJavengers;
+package TheJavengers.modelo;
+
 /**
  * Clase abstracta que representa un socio del centro excursionista.
  * Proporciona la estructura básica para definir un socio, incluyendo los atributos comunes
