b999a2e5d7e0544bda37358995db79e67f120a40
Admin <diegovaloriza@gmail.com>
2024-10-16 17:56:35 +0200

    Commit Fase 2

--- TheJavengers_IntellJ/src/TheJavengers/Escapadas.java
+++ TheJavengers_IntellJ/src/TheJavengers/Escapadas.java
@@ -2,7 +2,7 @@ package TheJavengers;
 
 public class Escapadas {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo6");
         System.out.println("Hola Mundo");
         System.out.println("Hola Mundo");
         System.out.println("Hola Mundo 5");
