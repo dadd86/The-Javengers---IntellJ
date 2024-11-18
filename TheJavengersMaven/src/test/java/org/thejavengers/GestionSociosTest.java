a28590981c8785a2b33a6de2cb908928e5d56e97
ramoralesloz <ramoralesloz@gmail.com>
2024-11-18 20:00:28 +0100

    Guardar todos los cambios en master

--- TheJavengersMaven/src/test/java/org/thejavengers/GestionSociosTest.java
+++ TheJavengersMaven/src/test/java/org/thejavengers/GestionSociosTest.java
@@ -1,4 +1,4 @@
-package org.thejavengers; // Ajustado al paquete correcto
+package src.test.java.org.thejavengers; // Ajustado al paquete correcto
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
